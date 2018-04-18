c45398a8da586eeb4f06d70e7ccd491dfac4735b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 04:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function placeOrder(cardNumber) {
   }
   else{
     cart = []
-    return `Your total cost is $` +
+    return `Your total cost is $` + 
     `${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
