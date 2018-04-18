c812ba06170fb9e0ac869d750c058f4694873875
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 03:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   var cart = "In your cart, you have "
+
+  if(cart.length == 0){
+    return "Your shopping cart is empty."
+  }
   for(var i =0; i < cart.length;i++)
   {
     if(i == cart.length - 1){
