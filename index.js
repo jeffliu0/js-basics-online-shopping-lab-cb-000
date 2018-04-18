b18bdc83851152b5961408a0c595fb4a6b42465e
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 04:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 
   for(var i = 0; i < cart.length; i++){
     if(cart[i].itemName == item){
-      delete cart[i]
+    cart.splice(i, 1)
       return cart
     }
   }
